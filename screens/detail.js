c4ad24a68caf899248b61efc9f79e932aaad2c9d
Nuwangsn <nuwansameeragsn@gmail.com>
2021-04-26 19:18:09 +0530

    new files created for the basic screens

---
+++ screens/detail.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const detail = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default detail
+
+const styles = StyleSheet.create({})
